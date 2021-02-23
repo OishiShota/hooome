65e52efd1018b9f1dcf61f5a6c247cf947a34884
OishiShota <s92.oishi.shota@gmail.com>
2021-02-23 12:23:07 +0900

    検索数

--- app/services/search_homes_service.rb
+++ app/services/search_homes_service.rb
@@ -3,7 +3,7 @@ class SearchHomesService
     if search != ""
       if search.start_with?("@")
         search = "#{search}%"
-        Home.find_by_sql(["SELECT * FROM users WHERE address LIKE ?", search])
+        User.find_by_sql(["SELECT * FROM users WHERE address LIKE ?", search])
       else
         search = "%#{search}%"
         Home.find_by_sql(["SELECT * FROM homes WHERE name LIKE ? ", search])
