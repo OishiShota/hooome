7e4007f2d1567c1f7176015ff6e3c57d2e858eef
OishiShota <s92.oishi.shota@gmail.com>
2021-02-24 19:01:55 +0900

    コメント履歴の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    query = "SELECT * FROM homes WHERE user_id = #{(params[:id])} "
+    query = "SELECT * FROM homes WHERE user_id = #{(params[:id])} ORDER BY updated_at DESC"
     @user_homes = Home.find_by_sql(query)
+    query = "SELECT h.id,h.name,h.user_id,h.created_at,h.updated_at FROM homes h JOIN messages m ON h.id = m.home_id WHERE m.user_id = 1 GROUP BY h.id ORDER BY updated_at DESC;"
+    @user_message_homes = Home.find_by_sql(query)
   end
 end
