0524e6f8e95ccfec971fdbebf05670e6280f13a2
OishiShota <s92.oishi.shota@gmail.com>
2021-02-24 19:06:00 +0900

    idの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     query = "SELECT * FROM homes WHERE user_id = #{(params[:id])} ORDER BY updated_at DESC"
     @user_homes = Home.find_by_sql(query)
-    query = "SELECT h.id,h.name,h.user_id,h.created_at,h.updated_at FROM homes h JOIN messages m ON h.id = m.home_id WHERE m.user_id = 1 GROUP BY h.id ORDER BY updated_at DESC;"
+    query = "SELECT h.id,h.name,h.user_id,h.created_at,h.updated_at FROM homes h JOIN messages m ON h.id = m.home_id WHERE m.user_id = #{(params[:id])} GROUP BY h.id ORDER BY updated_at DESC;"
     @user_message_homes = Home.find_by_sql(query)
   end
 end
