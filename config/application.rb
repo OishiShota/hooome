1fc06131bd4582870ebe4ebeb377b82eb33e40ec
OishiShota <s92.oishi.shota@gmail.com>
2021-02-25 12:44:02 +0900

    時間の日本語化

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,8 @@ module Hooome
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # 日本語の言語設定
-   config.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
